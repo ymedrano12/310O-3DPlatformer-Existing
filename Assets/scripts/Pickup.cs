c60caaec2e0cec964f576ec7456c98c0e57e3a1f
ymedrano12 <ymedrano@colostate.edu>
2026-03-11 21:04:26 -0600

    Submission

--- Assets/scripts/Pickup.cs
+++ Assets/scripts/Pickup.cs
@@ -5,6 +5,7 @@ public class Pickup : MonoBehaviour
     private void OnTriggerEnter(Collider other){
         if(other.CompareTag("Player")){
             Debug.Log("Pickup trigger entered");
+            FindFirstObjectByType<ScoreUI>().IncreaseScore();
             Destroy(gameObject);
         }
     }
